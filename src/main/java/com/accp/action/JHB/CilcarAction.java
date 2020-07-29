5b5dc796dd6e04c81898a27cb36cd15c0eb3d1cf
mywork <2068804453@qq.com>
2020-07-29 11:15:10 +0800

    第一次修改

---
+++ src/main/java/com/accp/action/JHB/CilcarAction.java
@@ -0,0 +1,22 @@
+package com.accp.action.JHB;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accp.biz.JHB.CilcarBiz;
+import com.accp.pojo.Cilcar;
+import com.github.pagehelper.PageInfo;
+
+@RestController
+@RequestMapping("/api/cilcar")
+public class CilcarAction {
+	@Autowired
+	private CilcarBiz cb;
+	@GetMapping("/selectAll/{pageNum}/{pageSize}")
+	public PageInfo<Cilcar> selectAll(@PathVariable Integer pageNum,@PathVariable Integer pageSize){
+		return cb.selectAll(pageNum, pageSize);
+	}
+}
