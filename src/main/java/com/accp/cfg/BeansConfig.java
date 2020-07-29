2c77a70c37b928c8e8b944f0f4628ca6687e9ef8
mywork <2068804453@qq.com>
2020-07-29 10:28:51 +0800

    焦浩博

--- src/main/java/com/accp/cfg/BeansConfig.java
+++ src/main/java/com/accp/cfg/BeansConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration   //配置类元注解
-@MapperScan(basePackages = { "com.accp.dao" }) //非常重要
+@MapperScan(basePackages = { "com.accp.dao" }) //非常重要  阿斯顿撒大所
 public class BeansConfig {
 	
 
